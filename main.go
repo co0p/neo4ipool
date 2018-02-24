52b84b406fdf37b75fb40038194e4b1e2e898b38
Julian Godesa <julian.godesa@googlemail.com>
2018-02-24 21:07:22 +0100

    oh my god .... we have kirk on board

--- main.go
+++ main.go
@@ -1,7 +1,46 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"log"
+	"strings"
+
+	"github.com/jmcvetta/neoism"
+)
+
+var dbURL string
+var dbPassword string
+var dbUser string
+
+func init() {
+	flag.StringVar(&dbURL, "url", "", "url location of neo4j db")
+	flag.StringVar(&dbUser, "username", "", "username of db")
+	flag.StringVar(&dbPassword, "password", "", "password of db")
+}
 
 func main() {
-	fmt.Println("will I be a webservice?, will I be a cli?")
+	flag.Parse()
+
+	if len(dbURL) == 0 {
+		log.Fatalln("usage: go run main.go -url <neo4j url> -user <user> -password <password>")
+	}
+
+	fmt.Println("I AM a cli!")
+
+	data := []string{"https://", dbUser, ":", dbPassword, "@", dbURL}
+	urlWithCredentials := strings.Join(data, "")
+	db, err := neoism.Connect(urlWithCredentials)
+
+	if err != nil {
+		log.Fatalf("failed to connect to db: %s", err.Error())
+	}
+
+	n, err := db.CreateNode(neoism.Props{"name": "Captain Kirk"})
+	if err != nil {
+		log.Printf("failed to create node: %s", err.Error())
+	}
+
+	name, _ := n.Property("name")
+	log.Printf("CREATED NODE with name: %s\n", name)
 }
